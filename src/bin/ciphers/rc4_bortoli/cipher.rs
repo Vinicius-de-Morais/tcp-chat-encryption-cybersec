103306d1f3b2f6d84beed1d6361c52c9ceacbb39
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2025-09-01 16:06:07 -0300

    feat: rc4_bortoli - add tests
    same tests from the first rc4 algorithm

--- src/bin/ciphers/rc4_bortoli/cipher.rs
+++ src/bin/ciphers/rc4_bortoli/cipher.rs
@@ -87,3 +87,62 @@ impl Cipher for Rc4Bortoli {
         String::from_utf8_lossy(&ciphered).to_string()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rc4bortoli_case1() {
+        let key = "D&Ot)[YW";
+        let plaintext = "Cybersecurity melhor disciplina do curso.";
+        let expected_cipher = [
+            214, 32, 110, 109, 116, 251, 159, 133, 226, 76, 193, 253, 168, 73, 65, 197, 82, 72, 93,
+            68, 250, 55, 28, 202, 59, 77, 186, 27, 97, 24, 48, 54, 106, 38, 82, 214, 222, 20, 20,
+            13, 251,
+        ];
+        let mut rc4 = Rc4Bortoli::new(key.to_string());
+        let ciphered = plaintext
+            .as_bytes()
+            .iter()
+            .map(|b| rc4.process_single(*b))
+            .collect::<Vec<u8>>();
+        assert_eq!(ciphered, expected_cipher);
+    }
+
+    #[test]
+    fn test_rc4bortoli_case2() {
+        let key = "$@C*9)6C{4^dXNw>H#W,be/\\'L2pM8r;JY?x}B]@A`T!q?iO`=n.Lgm(3z8@S[u]dY1k|%RI!MP-(FtZl&^3:jnK<TG6[5Jw}";
+        let plaintext = "Cybersecurity melhor disciplina do curso.";
+        let expected_cipher = [
+            84, 179, 117, 15, 203, 82, 18, 217, 141, 197, 213, 126, 47, 255, 83, 83, 99, 47, 120,
+            247, 192, 203, 33, 247, 220, 192, 213, 82, 241, 248, 166, 142, 129, 105, 50, 227, 178,
+            74, 181, 144, 94,
+        ];
+        let mut rc4 = Rc4Bortoli::new(key.to_string());
+        let ciphered = plaintext
+            .as_bytes()
+            .iter()
+            .map(|b| rc4.process_single(*b))
+            .collect::<Vec<u8>>();
+        assert_eq!(ciphered, expected_cipher);
+    }
+
+    #[test]
+    fn test_rc4bortoli_case3() {
+        let key = "!M|7s]u^{DFj^?8+fL:0Z!*%1P_3B}9m~V0@H^Qf7y&Z4Wb>kS^T<d.$.pL@R|g)x)-6(E&h%T-}(W%z{U9mZz8~m8BfP!c&@k7I\\5I~T_vD!4A>|oO[}3*T|$?e~0]V5&y@r1X2k+@T]j?|2|Q%}R,D)Up\\8gM;W}|7eNFk^t.h/j;6#y-!t5)\\^LJ[7S<4A,f$Ks1|&sX!w*G(Z@i>jE>6~]oA5]k'.:o=7n9h)$J_!aB{N-Jb1M}NzD\\*h";
+        let plaintext = "Cybersecurity melhor disciplina do curso.";
+        let expected_cipher = [
+            192, 115, 138, 155, 179, 72, 115, 33, 116, 105, 228, 122, 36, 92, 74, 122, 123, 245,
+            202, 209, 214, 199, 4, 191, 90, 96, 21, 15, 190, 222, 47, 58, 192, 192, 43, 10, 166,
+            63, 58, 96, 230,
+        ];
+        let mut rc4 = Rc4Bortoli::new(key.to_string());
+        let ciphered = plaintext
+            .as_bytes()
+            .iter()
+            .map(|b| rc4.process_single(*b))
+            .collect::<Vec<u8>>();
+        assert_eq!(ciphered, expected_cipher);
+    }
+}
